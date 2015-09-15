f70e63bcfef026d3c6aff91c72498a1c2fdef231
Gabor Makrai <makraigabor@gmail.com>
2015-09-15 16:59:32 +0100

    datapre-tools: ads comment

--- datapre-tools/src/MapCoordinate.py
+++ datapre-tools/src/MapCoordinate.py
@@ -1,4 +1,8 @@
 class MapCoordinate:
+    """
+    This class represents a coordinate in the Web Marcator projection system
+    x and y are between 0.0 and 512.0 
+    """
     def __init__(self, x, y):
         self.x = x
         self.y = y
--- datapre-tools/src/WGS84Coordinate.py
+++ datapre-tools/src/WGS84Coordinate.py
@@ -1,6 +1,11 @@
 from geopy.distance import vincenty
 
 class WGS84Coordinate:
+    """
+    This class represents a coordinate in the WGS84 Projection system
+    latitude is between -90.0 and 90.0
+    longitude is between -180.0 and 180.0
+    """
     
     def __init__(self, latitude, longitude):
         self.latitude = latitude
