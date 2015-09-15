933b53ede9c801960d001b400fcdbc17a95a6bde
Gabor Makrai <makraigabor@gmail.com>
2015-09-15 17:52:33 +0100

    datapre-tools: ads constants for MathTools

--- datapre-tools/src/MathTools.py
+++ datapre-tools/src/MathTools.py
@@ -13,3 +13,10 @@ def bound(val, valMin, valMax):
     res = max(val, valMin)
     res = min(res, valMax)
     return res
+
+class MathTools:
+    TILE_SIZE = 256.0
+    ORIGIN_X = TILE_SIZE / 2.0
+    ORIGIN_Y = TILE_SIZE / 2.0
+    PIXELS_PER_LONGITUDE_DEGREE = TILE_SIZE / 360.0
+    PIXELS_PER_LONGITUDE_RADIAN = TILE_SIZE / (2.0 * math.pi)
