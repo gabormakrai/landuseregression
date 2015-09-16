571e4d45734cadd464a6f04a4be7ce1b28432aea
Gabor Makrai <makraigabor@gmail.com>
2015-09-16 13:23:10 +0100

    datapre-tools: contains working WGS84Coordinate and its test

--- datapre-tools/src/WGS84Coordinate.py
+++ datapre-tools/src/WGS84Coordinate.py
@@ -1,4 +1,6 @@
 from geopy.distance import vincenty
+from MathTools import MathTools, bound, degreeToRadian
+import math
 
 class WGS84Coordinate:
     """
@@ -17,3 +19,21 @@ class WGS84Coordinate:
     def distance(self, otherCoordinate):
         return vincenty((self.latitude, self.longitude), (otherCoordinate.latitude, otherCoordinate.longitude)).meters
     
+    def toZoomMapCoordinate(self):
+        from ZoomMapCoordinate import ZoomMapCoordinate
+        x = MathTools.ORIGIN_X + self.longitude * MathTools.PIXELS_PER_LONGITUDE_DEGREE
+        siny = bound(math.sin(degreeToRadian(self.latitude)), -0.9999, 0.9999)
+        y = MathTools.ORIGIN_Y + 0.5 * math.log((1 + siny) / (1 - siny)) * - MathTools.PIXELS_PER_LONGITUDE_RADIAN
+        return ZoomMapCoordinate(x * 2.0, y * 2.0, 1)
+    
+    # Truncating to 0.9999 effectively limits latitude to 89.189. This is
+    # about a third of a tile past the edge of the world tile.
+    def toMapCoordinate(self):
+        from MapCoordinate import MapCoordinate
+        x = MathTools.ORIGIN_X + self.longitude * MathTools.PIXELS_PER_LONGITUDE_DEGREE
+        siny = bound(math.sin(degreeToRadian(self.latitude)), -0.9999, 0.9999)
+        y = MathTools.ORIGIN_Y + 0.5 * math.log((1 + siny) / (1 - siny)) * - MathTools.PIXELS_PER_LONGITUDE_RADIAN
+        return MapCoordinate(x * 2.0, y * 2.0)
+    
+    def toWGS84Coordinate(self):
+        return self
--- datapre-tools/test/WGS84CoordinateTest.py
+++ datapre-tools/test/WGS84CoordinateTest.py
@@ -14,3 +14,15 @@ barcelona = WGS84Coordinate(41.3879169, 2.16991870)
 madrid = WGS84Coordinate(40.4167413, -3.7032498)
 
 print("distance(barcelona, madrid) = " + str(barcelona.distance(madrid)))
+
+
+c1 = WGS84Coordinate(66.51326044311185,-90.0)
+c2 = c1.toMapCoordinate()
+c3 = c1.toZoomMapCoordinate()
+c4 = c1.toWGS84Coordinate()
+
+print("c1: " + c1.toString())
+print("c2: " + c2.toString())
+print("c3: " + c3.toString())
+print("c4: " + c4.toString())
+
