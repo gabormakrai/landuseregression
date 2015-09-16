9d53e5efef47650ec68f1d89886595f55ade090b
Gabor Makrai <makraigabor@gmail.com>
2015-09-16 13:18:18 +0100

    datapre-tools: contains working MapCoordinate and MapCoordinateTest

--- datapre-tools/src/MapCoordinate.py
+++ datapre-tools/src/MapCoordinate.py
@@ -1,11 +1,33 @@
+from MathTools import MathTools, radianToDegree
+import math
+
 class MapCoordinate:
+    
     """
     This class represents a coordinate in the Web Marcator projection system
     x and y are between 0.0 and 512.0 
     """
+    
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        
     def toString(self):
         return "MapCoordinate(x:" + str(self.x) + ",y:" + str(self.y) + ")"
 
+    def toWGS84Coorinate(self):
+        from WGS84Coordinate import WGS84Coordinate
+        lng = (self.x / 2.0 - MathTools.ORIGIN_X) / MathTools.PIXELS_PER_LONGITUDE_DEGREE
+        latRadians = (self.y / 2.0 - MathTools.ORIGIN_Y) / - MathTools.PIXELS_PER_LONGITUDE_RADIAN
+        lat = radianToDegree(2.0 * math.atan(math.exp(latRadians)) - math.pi / 2.0)
+        return WGS84Coordinate(lat, lng)
+    
+    def toMapCoordinate(self):
+        return self
+    
+    def toZoomMapCoordinate(self):
+        from ZoomMapCoordinate import ZoomMapCoordinate
+        return ZoomMapCoordinate(self.x, self.y, 1)
+    
+    def distance(self, otherCoordinate):
+        return self.toWGS84Coorinate().distance(otherCoordinate)
--- datapre-tools/test/MapCoordinateTest.py
+++ datapre-tools/test/MapCoordinateTest.py
@@ -1,5 +1,21 @@
 from MapCoordinate import MapCoordinate
 
 c = MapCoordinate(5.0, 12.0)
+print("c: " + c.toString())
 
-print(c.toString())
+c1 = MapCoordinate(128.0, 128.0)
+c2 = c1.toWGS84Coorinate()
+print("c1: " + c1.toString())
+print("c2: " + c2.toString())
+
+c3 = MapCoordinate(256.0, 256.0)
+c4 = c3.toWGS84Coorinate()
+print("c3: " + c3.toString())
+print("c4: " + c4.toString())
+
+c5 = c1.toZoomMapCoordinate()
+print("c5: " + c5.toString())
+
+c6 = c1.toMapCoordinate()
+print("c6: " + c6.toString())
+
