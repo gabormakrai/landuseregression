e002ce07828b80d1e3bc0494eed179097fc5c038
Gabor Makrai <makraigabor@gmail.com>
2015-09-21 13:06:44 +0100

    datapre-tools: Geometry contains pointsInTriangle function

--- datapre-tools/src/Geometry.py
+++ datapre-tools/src/Geometry.py
@@ -24,12 +24,45 @@ def linelineIntersection(a, b, c, d):
     ca = Vector(vectorC.x - vectorA.x, vectorC.y - vectorA.y)
     ab = Vector(vectorB.x - vectorA.x, vectorB.y - vectorA.y)
     cd = Vector(vectorD.x - vectorC.x, vectorD.y - vectorC.y)
+    ac = Vector(vectorA.x - vectorC.x, vectorA.y - vectorC.y)
     caXcd = ca.crossProduct(cd)
     abXcd = ab.crossProduct(cd)
-    if abs(abXcd) - 0.00000000001 < 0:
+    acXab = ac.crossProduct(ab)
+    cdXab = cd.crossProduct(ab)
+    if abs(abXcd) - 0.0000000000001 < 0:
         return None 
     t = caXcd / abXcd
-    if t > 0 and t < 1:
+    u = acXab / cdXab
+    if t > 0 and t < 1 and u > 0 and u < 1:
+#        print("t: " + str(t))
         return Vector(vectorA.x + t * ab.x, vectorA.y + t * ab.y)
     else:
         return None
+    
+def sign(p1x, p1y, p2x, p2y, p3x, p3y):
+    return ((p1x - p3x) * (p2y - p3y) - (p2x - p3x) * (p1y - p3y)) < 0.0
+
+def pointInTriangle(a, b, c, p):
+    
+        x1 = a.x
+        x2 = b.x
+        x3 = c.x
+        y1 = a.y
+        y2 = b.y
+        y3 = c.y
+        x = p.x
+        y = p.y
+        
+        b1 = sign(x, y, x1, y1, x2, y2)
+        b2 = sign(x, y, x2, y2, x3, y3)
+        
+        if b1 != b2:
+            return False
+        
+        b3 = sign(x, y, x3, y3, x1, y1)
+        
+        if b2 != b3:
+            return False
+        
+        return True
+    
