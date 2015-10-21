35037603ff6783ab7a15557dcdd2675c2b32ceb6
Gabor Makrai <makraigabor@gmail.com>
2015-10-21 23:51:18 +0100

    model: ads correlation calculation

--- model/src/eval/correlation.py
+++ model/src/eval/correlation.py
@@ -1,6 +1,30 @@
+import math
 
 # calculating correlation
-def evalCorrelation(data, target, prediction):
-    print("Not yet implemented")
-    return 0.0
-
+def correlationEval(targetData, predictionData):
+    
+    averageTarget = 0.0
+    averagePrediction = 0.0
+    
+    for i in range(0, len(targetData)):
+        averageTarget = averageTarget + targetData[i]
+        averagePrediction = averagePrediction + predictionData[i]
+        
+    averageTarget = averageTarget / float(len(targetData))
+    averagePrediction = averagePrediction / float(len(targetData))
+    
+    r = 0.0
+    varianceTarget = 0.0
+    variancePrediction = 0.0
+    
+    for i in range(0, len(targetData)):
+        r = r + (targetData[i] - averageTarget) * (predictionData[i] - averagePrediction)
+        varianceTarget = varianceTarget + math.pow(targetData[i] - averageTarget, 2.0)
+        variancePrediction = variancePrediction + math.pow(predictionData[i] - averagePrediction, 2.0)
+    
+    stddevTarget = math.sqrt(varianceTarget);
+    stddevPrediction = math.sqrt(variancePrediction);
+    
+    r = r / (stddevTarget * stddevPrediction);
+    
+    return r
