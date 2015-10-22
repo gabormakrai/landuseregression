782950d7f21c21fd5e9133a0b6216d32e0e84b49
Gabor Makrai <makraigabor@gmail.com>
2015-10-23 00:16:36 +0100

    model: adds updated X-validation code

--- model/src/crossvalidation.py
+++ model/src/crossvalidation.py
@@ -3,7 +3,7 @@ File contains the code for splitting the data for Cross-Validation
 """
 import random
 
-def crossValidation(k, data, columns, targetColumn, trainFunction, applyFunction, evalFunction):
+def crossValidation(k, data, columns, targetColumn, normalization, trainFunction, applyFunction, evalFunction):
     # create group class data
     group = []
     dataCounter = {}
@@ -14,8 +14,8 @@ def crossValidation(k, data, columns, targetColumn, trainFunction, applyFunction
         group.append(g)
         dataCounter[g] = dataCounter[g] + 1
         
-    print(dataCounter)
-    print(columns)
+    print("Columns: " + str(columns))
+    print("\tGroups: " + str(dataCounter))
         
     for iteration in range(0, k):        
         trainData = {}
@@ -34,9 +34,17 @@ def crossValidation(k, data, columns, targetColumn, trainFunction, applyFunction
             for column in columns:
                 targetData[column].append(data[column][i])
                 
+        normalization.stat(trainData)
+        
+        normalization.normalize(trainData)
+        
+        normalization.normalize(testData)
+                
         model = trainFunction(trainData, columns, targetColumn)
         
         predictionData = applyFunction(testData, model)
+        
+        normalization.denormalize(predictionData, targetColumn)
         
         m = evalFunction(testData[targetColumn], predictionData)
         
