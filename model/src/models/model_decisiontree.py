83b5ed1e78f62c901f3f34ae30b071d902d004a1
Gabor Makrai <makraigabor@gmail.com>
2015-12-01 12:17:21 +0000

    model: adds decision tree model

---
+++ model/src/models/model_decisiontree.py
@@ -0,0 +1,46 @@
+
+from sklearn.tree import DecisionTreeRegressor
+
+class DecisionTreeModel:
+    def __init__(self, model, modelColumns):
+        self.model = model
+        self.modelColumns = modelColumns
+
+def trainDecisionTree(data, columns, targetColumn, parameters):
+    
+    modelColumns = []
+    for column in columns:
+        if column != targetColumn:
+            modelColumns.append(column)
+            
+    modelData = []
+    
+    for i in range(0, len(data[targetColumn])):
+        record = []
+        for column in modelColumns:
+            record.append(data[column][i])
+
+        modelData.append(record)
+    if "depth" in parameters:
+        model = DecisionTreeRegressor(max_depth = parameters["depth"])
+    elif "leaf" in parameters:
+        model = DecisionTreeRegressor(min_samples_leaf = parameters["leaf"])
+    
+    model.fit (modelData, data[targetColumn])
+    
+    return DecisionTreeModel(model, modelColumns)
+    
+def applyDecisionTree(data, model, parameters):
+    
+    applyData = []
+    
+    for i in range(0, len(data[model.modelColumns[0]])):
+        record = []
+        for column in model.modelColumns:
+            record.append(data[column][i])
+
+        applyData.append(record)
+    
+    x = model.model.predict(applyData)
+    
+    return x
