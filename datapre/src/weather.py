d23abb3ee1eb5d1befdcb20c50ee73154aee5bad
gabormakrai <makraigabor@gmail.com>
2016-08-11 13:00:11 +0100

    datapre: adds WUnderground data preprocessing to weather.py

--- datapre/src/weather.py
+++ datapre/src/weather.py
@@ -1,4 +1,8 @@
+import json
+from urllib.request import urlopen
 from rectangles import loadRectangles
+from time import sleep
+import os
 
 def processWeatherFile(inputFile, inputRectangleFile, outputFile, printPrefixString):
     
@@ -143,3 +147,270 @@ def processWeatherFileBinned(inputFile, inputRectangleFile, outputFile, printPre
     output.close()
 
     print(printPrefixString + "Done...")
+
+def downloadWeatherDataFromWunderground(apiKey, country, city, dates, outputDirectory, sleepTime, printPrefixString = ""):
+    for d in dates:
+        outputFile = outputDirectory + city.lower() + "_" + d + ".json"
+        # if outputFile exist then skip the process
+        if os.path.isfile(outputFile):
+            print(printPrefixString + "File " + outputFile + " does exist...")
+        else:
+            url = "http://api.wunderground.com/api/" + apiKey + "/history_" + d + "/q/" + country + "/" + city + ".json"
+            print(printPrefixString + "Downloading " + url + "...")
+            response = urlopen(url).read().decode("utf-8")
+            data = str(response)
+            print(printPrefixString + "Saving date to " + outputFile + "...")
+            output = open(outputFile, 'w')
+            output.write(data)
+            output.close()
+            print(printPrefixString + "Done...")
+            sleep(sleepTime)
+
+def downloadWeatherForecastFromWunderground(apiKey, country, city, outputFile, sleepTime, printPrefixString = ""):
+    url = "http://api.wunderground.com/api/" + apiKey + "/hourly10day/q/" + country + "/" + city + ".json"
+    print(printPrefixString + "Downloading " + url + "...")
+    response = urlopen(url).read().decode("utf-8")
+    data = str(response)
+    print(printPrefixString + "Saving date to " + outputFile + "...")
+    output = open(outputFile, 'w')
+    output.write(data)
+    output.close()
+    print(printPrefixString + "Done...")
+    sleep(sleepTime)
+
+def processWUData(timestamps, inputDirectory, rectangleFile, outputFile, printPrefixString = "", fileList = None, binned=False):
+    
+    # load rectangles
+    rectangles = []
+    # load rectangle data
+    loadRectangles(rectangles, rectangleFile, printPrefixString)
+    
+    weatherData = {}
+    
+    if fileList == None:
+        fileNames = next(os.walk(inputDirectory))[2]
+    else:
+        fileNames = fileList
+        
+    for fileName in fileNames:
+        
+        absoluteFileName = inputDirectory + fileName
+        with open(absoluteFileName, encoding='utf-8') as data_file:
+            data = json.loads(data_file.read())
+      
+        if "history" not in data:
+            continue
+      
+        observations = len(data['history']['observations'])
+        
+        for o in range(0, observations):
+            hour = str(data['history']['observations'][o]['date']['hour'])
+            day = str(data['history']['observations'][o]['date']['mday'])
+            month = str(data['history']['observations'][o]['date']['mon'])
+            year = str(data['history']['observations'][o]['date']['year'])
+            timestampKey = year + month + day + hour
+                        
+            winddirection = str(data['history']['observations'][o]['wdird'])
+            windspeed = str(data['history']['observations'][o]['wspdm'])
+            temperature = str(data['history']['observations'][o]['tempm'])
+            rain = str(data['history']['observations'][o]['rain'])
+            pressure = str(data['history']['observations'][o]['pressurem'])
+            humidity = str(data['history']['observations'][o]['hum'])
+            
+            weatherData[timestampKey] = {}
+            weatherData[timestampKey]['winddirection'] = winddirection
+            weatherData[timestampKey]['windspeed'] = windspeed
+            weatherData[timestampKey]['temperature'] = temperature
+            weatherData[timestampKey]['rain'] = rain
+            weatherData[timestampKey]['pressure'] = pressure
+            weatherData[timestampKey]['humidity'] = humidity
+            
+    output = open(outputFile, 'w')
+    if binned==False:
+        output.write("location,timestamp,winddirection,windspeed,temperature,humidity,rain,pressure\n")
+    else:
+        output.write("location,timestamp,")
+        for i in range(0, 12):
+            output.write("winddirection" + str(i) + ",")
+        for i in range(0, 8):
+            output.write("windspeed" + str(i) + ",")
+        for i in range(0, 6):
+            output.write("temperature" + str(i) + ",")
+        for i in range(0, 10):
+            output.write("humidity" + str(i) + ",")
+        for i in range(0,2):
+            output.write("rain" + str(i) + ",")
+        output.write("pressure0")
+        for i in range(1, 8):
+            output.write(",pressure" + str(i))
+        output.write("\n")
+    
+    for timestamp in timestamps:
+        if timestamp.key not in weatherData:
+            continue
+        if weatherData[timestamp.key]['pressure'] == "":
+            continue
+        if weatherData[timestamp.key]['humidity'] == "":
+            continue
+        if float(weatherData[timestamp.key]['pressure']) < 800 or float(weatherData[timestamp.key]['pressure']) > 1200:
+            continue
+        if weatherData[timestamp.key]['winddirection'] == "":
+            continue
+        if weatherData[timestamp.key]['windspeed'] == "":
+            continue
+        if weatherData[timestamp.key]['rain'] == "":
+            continue
+        try:
+            float(weatherData[timestamp.key]['humidity'])
+        except:
+            continue
+        
+        for rectangle in rectangles:
+            output.write(rectangle.ID)
+            output.write(",")
+            output.write(timestamp.key)
+            
+            if binned == False:
+            
+                output.write(",")
+                output.write(weatherData[timestamp.key]['winddirection'])
+                output.write(",")
+                output.write(weatherData[timestamp.key]['windspeed'])
+                output.write(",")
+                output.write(weatherData[timestamp.key]['temperature'])
+                output.write(",")
+                output.write(weatherData[timestamp.key]['humidity'])
+                output.write(",")
+                output.write(weatherData[timestamp.key]['rain'])
+                output.write(",")
+                output.write(weatherData[timestamp.key]['pressure'])
+                output.write("\n")
+            else:
+                # winddirection (30 degrees -> 12 variables)
+                category = int(float(weatherData[timestamp.key]['winddirection']) / 30.0)
+                for i in range(0, 12):
+                    if i == category:
+                        output.write(",1")
+                    else:
+                        output.write(",0")
+                # windspeed (5 m/s bins -> 8 variables)
+                category = int(float(weatherData[timestamp.key]['windspeed']) / 5.0)
+                for i in range(0, 8):
+                    if i == category:
+                        output.write(",1")
+                    else:
+                        output.write(",0")
+                # temperature (6 celsius degrees -> 6 variables)
+                category = int(float(weatherData[timestamp.key]['temperature']) / 5.0)
+                for i in range(0, 6):
+                    if i == category:
+                        output.write(",1")
+                    else:
+                        output.write(",0")
+                # humidity
+                category = int(float(weatherData[timestamp.key]['humidity']) / 10.0)
+                for i in range(0, 10):
+                    if i == category:
+                        output.write(",1")
+                    else:
+                        output.write(",0")
+                # rain
+                category = int(weatherData[timestamp.key]['rain'])
+                for i in range(0, 2):
+                    if i == category:
+                        output.write(",1")
+                    else:
+                        output.write(",0")
+                # pressure
+                category = int((float(weatherData[timestamp.key]['pressure']) - 955.0) / 10.0)
+                for i in range(0, 8):
+                    if i == category:
+                        output.write(",1")
+                    else:
+                        output.write(",0")
+                output.write("\n")
+    
+    output.close()
+    
+def appendForecastData(timestamps, inputFile, rectangleFile, outputFile, printPrefixString = ""):
+
+    # load rectangles
+    rectangles = []
+    # load rectangle data
+    loadRectangles(rectangles, rectangleFile, printPrefixString)    
+    
+    weatherData = {}
+    
+    with open(inputFile, encoding='utf-8') as data_file:
+        data = json.loads(data_file.read())
+    
+    forecastData = data['hourly_forecast']
+    
+    for forecast in forecastData:
+        
+        hour = str(forecast["FCTTIME"]["hour_padded"])
+        day = str(forecast["FCTTIME"]["mday_padded"])
+        month = str(forecast["FCTTIME"]["mon_padded"])
+        year = str(forecast["FCTTIME"]["year"])
+        timestampKey = year + month + day + hour
+                    
+        winddirection = str(forecast["wdir"]["degrees"])
+        windspeed = str(forecast["wspd"]["metric"])
+        temperature = str(forecast["temp"]["metric"])
+        rainQpf = float(forecast["qpf"]["metric"])
+        humidity = str(float(forecast["humidity"]) / 100.0)
+        
+        if rainQpf > 0.0:
+            rain = "1"
+        else:
+            rain = "0"
+        pressure = str(forecast["mslp"]["metric"])
+        
+#         print("ts(" + timestampKey + "):wd:" + str(winddirection) + ",ws:" + str(windspeed) + ",t:" + str(temperature) + ",r:" + str(rain) + ",p:" + str(pressure))        
+        
+        weatherData[timestampKey] = {}
+        weatherData[timestampKey]['winddirection'] = winddirection
+        weatherData[timestampKey]['windspeed'] = windspeed
+        weatherData[timestampKey]['temperature'] = temperature
+        weatherData[timestampKey]['rain'] = rain
+        weatherData[timestampKey]['pressure'] = pressure
+        weatherData[timestampKey]['humidity'] = humidity
+        
+    output = open(outputFile, 'a')
+    
+    for timestamp in timestamps:
+        if timestamp.key not in weatherData:
+            continue
+        if weatherData[timestamp.key]['pressure'] == "":
+            continue
+        if float(weatherData[timestamp.key]['pressure']) < 800 or float(weatherData[timestamp.key]['pressure']) > 1200:
+            continue
+        if weatherData[timestamp.key]['winddirection'] == "":
+            continue
+        if weatherData[timestamp.key]['windspeed'] == "":
+            continue
+        if weatherData[timestamp.key]['rain'] == "":
+            continue
+        if weatherData[timestamp.key]['humidity'] == "":
+            continue
+        
+        for rectangle in rectangles:
+            output.write(rectangle.ID)
+            output.write(",")
+            output.write(timestamp.key)
+            output.write(",")
+            output.write(weatherData[timestamp.key]['winddirection'])
+            output.write(",")
+            output.write(weatherData[timestamp.key]['windspeed'])
+            output.write(",")
+            output.write(weatherData[timestamp.key]['temperature'])
+            output.write(",")
+            output.write(weatherData[timestamp.key]['humidity'])
+            output.write(",")
+            output.write(weatherData[timestamp.key]['rain'])
+            output.write(",")
+            output.write(weatherData[timestamp.key]['pressure'])
+            output.write("\n")
+        
+    output.close()
+    
