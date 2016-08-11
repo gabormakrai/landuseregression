13bdccc41c017d30a219a22d8c525d06f1db6a75
gabormakrai <makraigabor@gmail.com>
2016-08-11 12:55:32 +0100

    datapre: updates airquality preprocessor

--- datapre/src/airquality.py
+++ datapre/src/airquality.py
@@ -1,5 +1,6 @@
 from rectangles import loadRectangles
 import os
+from urllib.request import urlopen
     
 def processAirQualityFiles(pollutant, years, rectangleFile, inputDirectory, printPrefixString = ""):
         
@@ -79,7 +80,10 @@ def loadFile(pollutant, fileName, data):
                     timeMinusOne = True
 
             if timeMinusOne:
-                hour = hour - 1
+                if hour == 0:
+                    hour = 23
+                else:
+                    hour = hour - 1
             
             if hour < 10:
                 hourString = "0" + str(hour)
@@ -134,4 +138,83 @@ def writeOutAnnualAverages(data, years, outputFile, printPrefixString = ""):
     output.close()
     
     print(printPrefixString + "Done...")
+
+def writeOutHourlyData(data, outputFile, printPrefixString = ""):
     
+    output = open(outputFile, 'w')
+    output.write("location,timestamp,target\n")    
+    
+    for location in data:
+        for timestamp in data[location]:
+            output.write(str(location))
+            output.write(",")
+            output.write(str(timestamp))
+            output.write(",")
+            output.write(str(data[location][timestamp]))
+            output.write("\n")
+            
+    output.close()
+    
+def downloadYorkAirqualityData(firstTimestamp, lastTimestamp, rectangleFile, printPrefixString = ""):
+
+    # load rectangles
+    rectangles = []
+    loadRectangles(rectangles, rectangleFile, printPrefixString)
+    
+    startDate = firstTimestamp.key[0:4] + "-" + firstTimestamp.key[4:6] + "-" + firstTimestamp.key[6:8]
+    endDate = lastTimestamp.key[0:4] + "-" + lastTimestamp.key[4:6] + "-" + lastTimestamp.key[6:8]
+    
+    urls = {}
+    urls["Heworth"] = "http://www.airqualityengland.co.uk/site/data.php?site_id=YK13&parameter_id%5B%5D=NO2&f_date_started=" + startDate + "&f_date_ended=" + endDate + "&la_id=76&action=download"
+    urls["Bootham"] = "http://www.airqualityengland.co.uk/site/data.php?site_id=YK10&parameter_id%5B%5D=NO2&f_date_started=" + startDate + "&f_date_ended=" + endDate + "&la_id=76&action=download"
+    urls["Fulford"] = "http://www.airqualityengland.co.uk/site/data.php?site_id=YK16&parameter_id%5B%5D=NO2&f_date_started=" + startDate + "&f_date_ended=" + endDate + "&la_id=76&action=download"
+    urls["Gillygate"] = "http://www.airqualityengland.co.uk/site/data.php?site_id=YK7&parameter_id%5B%5D=NO2&f_date_started=" + startDate + "&f_date_ended=" + endDate + "&la_id=76&action=download"
+    urls["Holgate"] = "http://www.airqualityengland.co.uk/site/data.php?site_id=YK8&parameter_id%5B%5D=NO2&f_date_started=" + startDate + "&f_date_ended=" + endDate + "&la_id=76&action=download"
+    urls["Lawrence"] = "http://www.airqualityengland.co.uk/site/data.php?site_id=YK9&parameter_id%5B%5D=NO2&f_date_started=" + startDate + "&f_date_ended=" + endDate + "&la_id=76&action=download"
+    urls["Nunnery"] = "http://www.airqualityengland.co.uk/site/data.php?site_id=YK15&parameter_id%5B%5D=NO2&f_date_started=" + startDate + "&f_date_ended=" + endDate + "&la_id=76&action=download"
+    
+    aqData = {}
+    
+    for rectangle in rectangles:
+        station = rectangle.name
+        if station == "Fishergate":
+            continue
+        
+        print(printPrefixString + "Downloading data for " + station + "...")
+ 
+        url = urls[station]
+        print(printPrefixString + "url: " + url)
+        response = urlopen(url).read().decode("utf-8")
+        r1 = response[response.find("<tr>") + 4:]
+        r1 = r1[r1.find("<tr>") + 4:]
+     
+        while True:
+             
+            if r1.find("<tr>") == -1:
+                break
+             
+            r1 = r1[r1.find("<tr>") + 4:]
+            r1 = r1[r1.find("<td>") + 4:]
+            dateString = r1[0:r1.find("</td>")]
+            r1 = r1[r1.find("<td") + 3:]
+            r1 = r1[r1.find(">") + 1:]
+            timeString = r1[0:r1.find("</td>")]
+            r1 = r1[r1.find("<td>") + 4:]
+            levelString = r1[0:r1.find("</td>")]
+            
+            # 16/06/2016
+            #24:00:00&nbsp;#
+            hour = int(timeString[0:2]) - 1
+            if hour < 10:
+                timestampKey = dateString[6:10] + dateString[3:5] + dateString[0:2] + "0" + str(hour)
+            else:
+                timestampKey = dateString[6:10] + dateString[3:5] + dateString[0:2] + str(hour)
+                
+            if timestampKey not in aqData:
+                aqData[timestampKey] = {}
+
+            try:
+                aqData[timestampKey][rectangle.ID] = float(levelString)
+            except:
+                continue
+    return aqData
