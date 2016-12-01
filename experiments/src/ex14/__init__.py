6bccd0b7b9ee48a2e9236a495be878805e97c182
gabormakrai <makraigabor@gmail.com>
2016-12-01 18:32:37 +0000

    adds ex14

---
+++ experiments/src/ex14/__init__.py
@@ -0,0 +1,8 @@
+################################################################################
+# Experiment No. 14
+# 
+# Weekly heatmaps with predictions and prediction errors for two models
+# model1: T+W
+# model2: T+W+Atc
+#
+
