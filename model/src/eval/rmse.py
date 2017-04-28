eefa08a87297a779072a90496033b62d1650e6ad
gabormakrai <makraigabor@gmail.com>
2017-04-28 18:58:37 +0100

    model: updates rmse eval for supporing NaN values

--- model/src/eval/rmse.py
+++ model/src/eval/rmse.py
@@ -2,10 +2,16 @@ import math
 
 # calculating rmse
 def rmseEval(targetData, predictionData):
+    
     # print(targetData)
     # print(predictionData)
+    
     rmse = 0.0
+    counter = 0
     for i in range(0, len(targetData)):
+        if math.isnan(targetData[i]) or math.isnan(predictionData[i]):
+            continue
         rmse = rmse + math.pow(targetData[i] - predictionData[i], 2.0)
-    rmse = math.sqrt(rmse / float(len(targetData)))
+        counter = counter + 1
+    rmse = math.sqrt(rmse / float(counter))
     return ["rmse", rmse]
