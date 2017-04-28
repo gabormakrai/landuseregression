95174ad7bff9c5ecce03696aa9253f50346a2fd1
gabormakrai <makraigabor@gmail.com>
2017-04-28 18:58:17 +0100

    model: updates mea code for supporting NaN values

--- model/src/eval/mae.py
+++ model/src/eval/mae.py
@@ -1,8 +1,13 @@
+import math
 
 # calculating mae
 def maeEval(targetData, predictionData):
     mae = 0.0
+    counter = 0
     for i in range(0, len(targetData)):
+        if math.isnan(targetData[i]) or math.isnan(predictionData[i]):
+            continue
         mae = mae + abs(targetData[i] - predictionData[i])
-    mae = mae / float(len(targetData))
+        counter = counter + 1
+    mae = mae / float(counter)
     return ["mae", mae]
