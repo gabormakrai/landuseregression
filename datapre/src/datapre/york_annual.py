ec55e51ff1447e0574ad77f3a919ce53939e1e36
Gabor Makrai <makraigabor@gmail.com>
2017-05-08 22:15:52 +0100

    datapre: updated york_annual.py

--- datapre/src/datapre/york_annual.py
+++ datapre/src/datapre/york_annual.py
@@ -6,107 +6,109 @@ from traffic import createRectangleTrafficAnnual
 from osmpolygons import getRectangleOSMPolygons
 from join import joinYear
 
-DATA_DIRECTORY = "/media/sf_lur/data/"
+DATA_DIRECTORY = "/home/makrai/data/york/"
+WORK_DIRECTORY = "/home/makrai/data/york_annual/"
+OUTPUT_DIRECTORY = "/home/makrai/data/" 
 
 years = [2012, 2013, 2014, 2015] 
 ranges = ["100", "250", "500"]
 
 print("Creating station rectangles in radius 100m, 250m, 500m...")
-  
+   
 for r in ranges:
     createStationRectangles(
         float(r), 
         DATA_DIRECTORY + "stations/stations.csv",
-        DATA_DIRECTORY + "stations/rectangles_" + r + ".csv",
-        DATA_DIRECTORY + "gis/rectangles_" + r + ".csv",
+        WORK_DIRECTORY + "rectangles_" + r + ".csv",
+        WORK_DIRECTORY + "gis/rectangles_" + r + ".csv",
         "\t")
-      
+       
 print("Done...")
-   
+    
 print("Parsing topo building data for all the radius (100,250,500) and for all years...")
- 
+  
 for year in years:
     for r in ranges:
         generateRectangleBuildings(
             DATA_DIRECTORY + "topo/buildings_" + str(year) + ".csv",
-            DATA_DIRECTORY + "stations/rectangles_" + r + ".csv",
+            WORK_DIRECTORY + "rectangles_" + r + ".csv",
             None,
             None,
-            DATA_DIRECTORY + "preprocessed_year/topo_" + r + "_" + str(year) + ".csv",
+            WORK_DIRECTORY + "topo_" + r + "_" + str(year) + ".csv",
             100,
             "\t")
-  
+   
 print("Done...")
-  
+   
 print("Loading data air quality data...")
-     
+      
 data = processAirQualityFiles(
     "no2", 
     years, 
-    DATA_DIRECTORY + "stations/rectangles_100.csv", 
+    WORK_DIRECTORY + "rectangles_100.csv", 
     DATA_DIRECTORY + "aq/", 
     "\t")
-    
+     
 print("Done...")
-    
+     
 print("Writing out annaul averages...")
-    
+     
 writeOutAnnualAverages(
     data, 
     years,
-    DATA_DIRECTORY + "preprocessed_year/aq.csv", 
+    WORK_DIRECTORY + "aq.csv", 
     "\t")
-    
+     
 print("Done...")
-   
+    
 print("Downloading osm data...")
-   
-downloadOsmData(-1.35, -0.83, 53.76, 54.09, DATA_DIRECTORY + "osm/downloaded/")
-   
-print("Done...")
-  
-print("Process osm data...")
     
+downloadOsmData(-1.35, -0.83, 53.76, 54.09, DATA_DIRECTORY + "osm/downloaded/")
+    
+print("Done...")
+   
+print("Process osm data...")
+     
 getPolygonsFromOSM(
     DATA_DIRECTORY + "osm/downloaded/", 
     DATA_DIRECTORY + "osm/history/", #historyDirectory 
-    DATA_DIRECTORY + "osm/polygons.csv", 
+    WORK_DIRECTORY + "polygons.csv", 
     "\t")
-    
+     
 print("Done...")
-   
-print("Process year osm polygon data...")
     
+print("Process year osm polygon data...")
+     
 for year in years:
     writeOutYearPolygons(
-        DATA_DIRECTORY + "osm/polygons.csv", 
+        WORK_DIRECTORY + "polygons.csv", 
         year, 
-        DATA_DIRECTORY + "osm/osmpoly_" + str(year) + ".csv", 
-        DATA_DIRECTORY + "gis/osmpoly_" + str(year) + ".csv", 
+        WORK_DIRECTORY + "osmpoly_" + str(year) + ".csv", 
+        WORK_DIRECTORY + "gis/osmpoly_" + str(year) + ".csv", 
         "\t")
-    
+     
 print("Done...")
-    
+     
 print("Process osm polygon data for station for each year...")
 for r in ["100", "250", "500"]:
     for year in years: 
         getRectangleOSMPolygons(
-            DATA_DIRECTORY + "osm/osmpoly_" + str(year) + ".csv",
-            DATA_DIRECTORY + "stations/rectangles_" + r + ".csv",
+            WORK_DIRECTORY + "osmpoly_" + str(year) + ".csv",
+            WORK_DIRECTORY + "rectangles_" + r + ".csv",
             "landuse",
             False,
-            DATA_DIRECTORY + "preprocessed_year/osmlanduse_" + r + "_" + str(year) + ".csv",
-            DATA_DIRECTORY + "temp5.txt",
+            WORK_DIRECTORY + "osmlanduse_" + r + "_" + str(year) + ".csv",
+            WORK_DIRECTORY + "gis/temp.txt",
             "\t")
         getRectangleOSMPolygons(
-            DATA_DIRECTORY + "osm/osmpoly_" + str(year) + ".csv",
-            DATA_DIRECTORY + "stations/rectangles_" + r + ".csv",
+            WORK_DIRECTORY + "osmpoly_" + str(year) + ".csv",
+            WORK_DIRECTORY + "rectangles_" + r + ".csv",
             "leisure",
             False,
-            DATA_DIRECTORY + "preprocessed_year/osmleisure_" + r + "_" + str(year) + ".csv",
-            DATA_DIRECTORY + "temp5.txt",
+            WORK_DIRECTORY + "osmleisure_" + r + "_" + str(year) + ".csv",
+            WORK_DIRECTORY + "gis/temp.txt",
             "\t")
-      
+       
 print("Done")
       
 print("Processing traffic information for station rectangles...")
@@ -114,9 +116,9 @@ for year in years:
     for r in ["100", "250", "500"]:      
         createRectangleTrafficAnnual(
             DATA_DIRECTORY + "traffic/traffic.csv",
-            DATA_DIRECTORY + "stations/rectangles_" + r + ".csv",
-            DATA_DIRECTORY + "preprocessed_year/traffic_" + r + "_" + str(year) + ".csv",
-            DATA_DIRECTORY + "temp3",
+            WORK_DIRECTORY + "rectangles_" + r + ".csv",
+            WORK_DIRECTORY + "traffic_" + r + "_" + str(year) + ".csv",
+            WORK_DIRECTORY + "gis/temp.txt",
             "\t")
        
 print("Done")
@@ -127,8 +129,8 @@ joinYear(
     ["traffic", "osmlanduse", "osmleisure", "topo"], 
     years, 
     ["100", "250", "500"], 
-    DATA_DIRECTORY + "preprocessed_year/",
-    DATA_DIRECTORY + "data_year.csv",
+    WORK_DIRECTORY,
+    OUTPUT_DIRECTORY + "york_year.csv",
     "\t")
     
 print("Done")
