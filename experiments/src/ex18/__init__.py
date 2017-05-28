c0dd5ae50e59b05a53b92d6d1a8dfbd35f57bf4c
Gabor Makrai <makraigabor@gmail.com>
2017-05-28 19:42:50 +0100

    experiments: adds comment to ex18

--- experiments/src/ex18/__init__.py
+++ experiments/src/ex18/__init__.py
@@ -1,5 +1,5 @@
 ################################################################################
 # Experiment No. 18
 # 
-# 
+# Error comparison for TW/TWA/WA models at special cases (morning vs afternoon)
 #
