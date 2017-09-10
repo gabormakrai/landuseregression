e1bf3f3a4f5473ba54f8e16be89f73e8c5d1b057
Gabor Makrai <makraigabor@gmail.com>
2017-09-10 22:31:09 +0100

    datapre: updated york_hour main script

--- datapre/src/datapre/york_hour.py
+++ datapre/src/datapre/york_hour.py
@@ -20,44 +20,46 @@ from airquality import processAirQualityFiles, writeOutHourlyData
 from atc import processAtcData
 from osmgrabber import getPolygonsFromOSM, writeOutYearPolygons, downloadOsmData
 
-DATA_DIRECTORY = "/home/makrai/data/york/"
-WORK_DIRECTORY = "/home/makrai/data/york_hour/"
-OUTPUT_DIRECTORY = "/home/makrai/data/"
+RAW_DATA_DIRECTORY = "/data_raw/york_hour/"
+WORK_DIRECTORY = "/data/york_hour/"
+OUTPUT_DIRECTORY = "/data/"
+
+SKIP_DOWNLOAD_STEPS = True
  
 years = [2012, 2013, 2014, 2015]
 
 print("Create rectangles for monitoring stations...")
-     
+      
 createStationRectangles(
     100.0, 
-    DATA_DIRECTORY + "stations/stations_withoutbootham.csv",
+    RAW_DATA_DIRECTORY + "stations/stations_withoutbootham.csv",
     WORK_DIRECTORY + "stations_rectangles.csv",
     WORK_DIRECTORY + "gis/station_rectangles.csv",
     "\t")
-     
+      
 print("Done...")
-    
-print("Creating traffic gis information file...")
      
+print("Creating traffic gis information file...")
+      
 createTrafficGISFile(
     WORK_DIRECTORY + "stations_rectangles.csv",
-    DATA_DIRECTORY + "traffic/traffic.csv",
+    RAW_DATA_DIRECTORY + "traffic/traffic.csv",
     WORK_DIRECTORY + "gis/traffic.csv",
     "\t")
-     
+      
 print("Done...")
-  
+   
 print("Processing traffic information for station rectangles...")
-     
+      
 createRectangleTraffic(
-    DATA_DIRECTORY + "traffic/traffic.csv",
+    RAW_DATA_DIRECTORY + "traffic/traffic.csv",
     WORK_DIRECTORY + "stations_rectangles.csv",
     WORK_DIRECTORY + "traffic.csv",
     WORK_DIRECTORY + "gis/stations_rectangles_traffic.csv",
     "\t")
-     
+      
 print("Done...")
-    
+     
 print("Add timestamps to traffic data...")
 for year in years:
     addTimestampToTraffic(
@@ -65,72 +67,75 @@ for year in years:
         WORK_DIRECTORY + "traffic.csv", 
         WORK_DIRECTORY + "traffic_" + str(year) + ".csv", 
         "\t")
-      
-print("Done...")
        
+print("Done...")
+        
 print("Processing OS Mastermap Topo layer for buildings...")
-    
+     
 generateAllBuildingGisInformation(
-    DATA_DIRECTORY + "topo/buildings_2012.csv",
+    RAW_DATA_DIRECTORY + "topo/buildings_2012.csv",
     WORK_DIRECTORY + "gis/buildings_2012.csv",
     "\t")
 generateAllBuildingGisInformation(
-    DATA_DIRECTORY + "topo/buildings_2013.csv",
+    RAW_DATA_DIRECTORY + "topo/buildings_2013.csv",
     WORK_DIRECTORY + "gis/buildings_2013.csv",
     "\t")
 generateAllBuildingGisInformation(
-    DATA_DIRECTORY + "topo/buildings_2014.csv",
+    RAW_DATA_DIRECTORY + "topo/buildings_2014.csv",
     WORK_DIRECTORY + "gis/buildings_2014.csv",
     "\t")
 generateAllBuildingGisInformation(
-    DATA_DIRECTORY + "topo/buildings_2015.csv",
+    RAW_DATA_DIRECTORY + "topo/buildings_2015.csv",
     WORK_DIRECTORY + "gis/buildings_2015.csv",
     "\t")
-    
-print("Done...")
      
+print("Done...")
+      
 print("Processing OS Mastermap Topo layer for buildings for station rectangles...")
 for year in years:   
     generateRectangleBuildings(
-        DATA_DIRECTORY + "topo/buildings_" + str(year) + ".csv",
+        RAW_DATA_DIRECTORY + "topo/buildings_" + str(year) + ".csv",
         WORK_DIRECTORY + "stations_rectangles.csv",
         WORK_DIRECTORY + "gis/station_buildings.csv",
         WORK_DIRECTORY + "gis/station_buildings_triangles.csv",
         WORK_DIRECTORY + "topo_" + str(year) + ".csv",
         100,
         "\t")
-                   
+                    
 print("Done...")
-    
+     
 print("Add timestamp to static topo building data...")
-    
+     
 for year in years: 
     multiplyBuildingData(
         WORK_DIRECTORY + "topo_" + str(year) + ".csv", 
         generateTimestamps(year), 
         WORK_DIRECTORY + "topo_timestamp_" + str(year) + ".csv", 
         "\t")
-      
+       
 print("Done...")
 
 print("Downloading osm data...")
-   
-downloadOsmData(-1.35, -0.83, 53.76, 54.09, DATA_DIRECTORY + "osm/downloaded/")
-   
+
+if SKIP_DOWNLOAD_STEPS:
+    print("\tSkipping download steps")
+else:    
+    downloadOsmData(-1.35, -0.83, 53.76, 54.09, WORK_DIRECTORY + "osm/downloaded/")
+    
 print("Done...")
-  
+   
 print("Processing downloaded osm data...")
-  
+   
 getPolygonsFromOSM(
-    DATA_DIRECTORY + "osm/downloaded/",
-    DATA_DIRECTORY + "osm/history/",
+    WORK_DIRECTORY + "osm/downloaded/",
+    WORK_DIRECTORY + "osm/history/",
     WORK_DIRECTORY + "polygons.csv",
     "\t")
-  
-print("Done...")
-  
-print("Genearing polygons for each year")
    
+print("Done...")
+   
+print("Genearing polygons for each year")
+    
 for year in years:
     writeOutYearPolygons(
         WORK_DIRECTORY + "polygons.csv", 
@@ -138,9 +143,9 @@ for year in years:
         WORK_DIRECTORY + "osmpoly_" + str(year) + ".csv", 
         WORK_DIRECTORY + "gis/osmpoly_" + str(year) + ".csv", 
         "\t")
-   
+    
 print("Done...")
-     
+      
 print("Processing osm data for landuses...")
 for year in years:
     getRectangleOSMPolygons(
@@ -159,9 +164,9 @@ for year in years:
         WORK_DIRECTORY + "osm_leisure_" + str(year) +".csv",
         WORK_DIRECTORY + "gis/stations_landuse.csv",                
         "\t")
-         
+          
 print("Done")
-      
+       
 print("Add timestamp to static osm poly data...")
 for year in years:
     multiplyLanduseData2(
@@ -175,20 +180,20 @@ for year in years:
         WORK_DIRECTORY + "osmleisure_" + str(year) + ".csv", 
         "\t")
 print("Done")
-         
+          
 print("Creating time related dataset...")
-    
+     
 for year in years:  
     createTimeFile(
         generateTimestamps(year), 
         WORK_DIRECTORY + "stations_rectangles.csv",
         WORK_DIRECTORY + "yorktime_" + str(year) +".csv",
         "\t")
-      
+       
 print("Done...")
-    
-print("Creating time related dataset (binned)...")
      
+print("Creating time related dataset (binned)...")
+      
 for year in years:  
     createTimeFile(
         generateTimestamps(year), 
@@ -196,89 +201,92 @@ for year in years:
         WORK_DIRECTORY + "yorktime2_" + str(year) +".csv",
         "\t",
         binned=True)
-       
+        
 print("Done...")
-    
+     
 print("Processing data air quality files...")
-    
+     
 for year in years:     
     print("Loading data air quality data...")
     data = processAirQualityFiles(
         "no2", 
         [year], 
         WORK_DIRECTORY + "stations_rectangles.csv", 
-        DATA_DIRECTORY + "aq/", 
+        WORK_DIRECTORY + "aq/", 
         "\t")
-        
+         
     print("Done...")
-        
+         
     print("Writing out hourly averages...")
-        
+         
     writeOutHourlyData(
         data,
         WORK_DIRECTORY + "airquality_" + str(year) + ".csv", 
         "\t")
-        
+         
     print("Done...")
-        
+         
 print("Done...")
-   
-print("Download weather data from WUnderground...")
     
-for year in years:
-    downloadWeatherDataFromWunderground(
-        "WUKEY", 
-        "UK", 
-        "York", 
-        generateDatesStringForYear(year), 
-        DATA_DIRECTORY + "weather/wu/", 
-        8.0,
-        "\t")
-      
-print("Done...")
+print("Download weather data from WUnderground...")
+
+if SKIP_DOWNLOAD_STEPS:
+    print("\tSkipping download steps...")
+else:
+    for year in years:
+        downloadWeatherDataFromWunderground(
+            "WUKEY", 
+            "UK", 
+            "York", 
+            generateDatesStringForYear(year), 
+            WORK_DIRECTORY + "weather/wu/", 
+            8.0,
+            "\t")
        
-print("Processing WU weather data...")
+print("Done...")
         
+print("Processing WU weather data...")
+         
 for year in years: 
     processWUData(
         generateTimestamps(year),
-        DATA_DIRECTORY + "weather/wu/", 
+        WORK_DIRECTORY + "weather/wu/", 
         WORK_DIRECTORY + "stations_rectangles.csv",
         WORK_DIRECTORY + "weather_" + str(year) + ".csv", 
         "\t")
-         
+          
 print("Done...")
-   
+    
 print("Processing WU weather data (binned)...")
-         
+          
 for year in years: 
     processWUData(
         generateTimestamps(year),
-        DATA_DIRECTORY + "weather/wu/", 
+        WORK_DIRECTORY + "weather/wu/", 
         WORK_DIRECTORY + "stations_rectangles.csv",
         WORK_DIRECTORY + "weather2_" + str(year) + ".csv", 
         "\t",
         fileList = None,
         binned=True)
-          
+           
 print("Done...")
-  
+   
 print("Processing ATC data...")
-  
+   
 for year in years:
     processAtcData(
         year,
         WORK_DIRECTORY + "stations_rectangles.csv", 
-        DATA_DIRECTORY + "atc/",
+        RAW_DATA_DIRECTORY + "atc/",
         WORK_DIRECTORY + "atc_" + str(year) + ".csv", 
         "\t")
-  
+   
 print("Done...")
-    
+     
 print("Joining preprocessed files...")
-         
+          
 for year in years:
-                  
+                   
     filesToJoin = [
         WORK_DIRECTORY + "traffic_" + str(year) + ".csv",
         WORK_DIRECTORY + "topo_timestamp_" + str(year) + ".csv",
@@ -287,19 +295,19 @@ for year in years:
         WORK_DIRECTORY + "airquality_" + str(year) + ".csv",
         WORK_DIRECTORY + "weather_" + str(year) + ".csv",
         WORK_DIRECTORY + "yorktime_" + str(year) + ".csv"]
-                  
+                   
     joinFiles(
         filesToJoin,
         OUTPUT_DIRECTORY + "york_hour_" + str(year) + ".csv",
         False,
         "\t")
-              
+               
 print("Done...")
-     
+      
 print("Joining preprocessed files (binned)...")
-           
+            
 for year in years:
-                    
+                     
     filesToJoin = [
         WORK_DIRECTORY + "traffic_" + str(year) + ".csv",
         WORK_DIRECTORY + "topo_timestamp_" + str(year) + ".csv",
@@ -308,19 +316,19 @@ for year in years:
         WORK_DIRECTORY + "airquality_" + str(year) + ".csv",
         WORK_DIRECTORY + "weather2_" + str(year) + ".csv",
         WORK_DIRECTORY + "yorktime2_" + str(year) + ".csv"]
-                    
+                     
     joinFiles(
         filesToJoin,
         OUTPUT_DIRECTORY + "york2_hour_" + str(year) + ".csv",
         False,
         "\t")
-               
+                
 print("Done...")
-  
+   
 print("Joining preprocessed files (w/ atc data)...")
-           
+            
 for year in [2013]:
-                    
+                     
     filesToJoin = [
         WORK_DIRECTORY + "traffic_" + str(year) + ".csv",
         WORK_DIRECTORY + "topo_timestamp_" + str(year) + ".csv",
@@ -331,11 +339,11 @@ for year in [2013]:
         WORK_DIRECTORY + "yorktime_" + str(year) + ".csv",
         WORK_DIRECTORY + "atc_" + str(year) + ".csv"
         ]
-                        
+                         
     joinFiles(
         filesToJoin,
         OUTPUT_DIRECTORY + "york3_hour_" + str(year) + ".csv",
         False,
         "\t")
-               
+                
 print("Done...")
