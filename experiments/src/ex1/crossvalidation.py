730d162d6feb8d7ec584123a967db49ae90d22e0
Gabor Makrai <makraigabor@gmail.com>
2017-10-07 15:33:44 +0100

    experiments: updating ex1.crossvalidation

--- experiments/src/ex1/crossvalidation.py
+++ experiments/src/ex1/crossvalidation.py
@@ -1,109 +1,33 @@
-"""
-File contains the code for splitting the data for Cross-Validation
-"""
-import random
-import math
+from copy import deepcopy
 
-def crossValidation(k, data, columns, targetColumn, normalization, trainFunction, applyFunction, evalFunctions, parameters):
-    # create group class data
-    group = []
-    dataCounter = {}
-    for i in range(0, k):
-        dataCounter[i] = 0
-    for i in range(0, len(data[targetColumn])):
-        g = random.randint(0, k - 1)
-        group.append(g)
-        dataCounter[g] = dataCounter[g] + 1
-        
-#     print("Columns: " + str(columns))
-#     print("\tGroups: " + str(dataCounter))
+def splitDataForXValidation(value, validationColumn, data, columns, targetColumn):
     
-    iterationResults = []
-    scatterResult = {}
-    scatterResult["prediction"] = []
-    scatterResult["test"] = []
+    columnsWithoutValidationAndTarget = deepcopy(columns)
+    if validationColumn in columnsWithoutValidationAndTarget:
+        columnsWithoutValidationAndTarget.remove(validationColumn)
+    if targetColumn in columnsWithoutValidationAndTarget:
+        columnsWithoutValidationAndTarget.remove(targetColumn)
+    
+    trainX = []
+    testX = []
+    trainY = []
+    testY = []
         
-    for iteration in range(0, k):        
-        trainData = {}
-        testData = {}
-        # add columns for trainData and testData
-        for column in columns:
-            trainData[column] = []
-            testData[column] = []
+    for i in range(0, len(data[validationColumn])):
+        targetX = trainX
+        targetY = trainY
+        if data[validationColumn][i] == value:
+            targetX = testX
+            targetY = testY
             
-        for i in range(0, len(group)):
-            if iteration == group[i]:
-                targetData = testData
-            else:
-                targetData = trainData
-    
-            for column in columns:
-                targetData[column].append(data[column][i])
-                
-        normalization.stat(trainData)
-        
-        normalization.normalize(trainData)
-        
-        normalization.normalize(testData)
-                
-        model = trainFunction(trainData, columns, targetColumn, parameters)
-        
-        predictionData = applyFunction(testData, model, parameters)
-        
-        normalization.denormalize(predictionData, targetColumn)
-        len(targetData)
-        normalization.denormalize(testData[targetColumn], targetColumn)
-        
-        for d in testData[targetColumn]:
-            scatterResult["test"].append(d)
-        for d in predictionData:
-            scatterResult["prediction"].append(d)
-        
-        resultArray = []
-        
-        for evalFunction in evalFunctions:
-            result = evalFunction(testData[targetColumn], predictionData)
-            resultArray.append(result)
+        X = []
+        for c in columnsWithoutValidationAndTarget:
+            X.append(data[c][i])
             
-        # print("\t" + str(resultArray))
+        Y = data[targetColumn][i]
         
-        iterationResults.append(resultArray)
-    
-    averages = {}
-    
-    for result in iterationResults:
-        for keyvalue in result:
-            key = keyvalue[0]
-            value = keyvalue[1]
-            if key not in averages:
-                averages[key] = 0.0
+        targetY.append(Y)
+        targetX.append(X)
             
-            current = averages[key]
-            current = current + value
-            averages[key] = current
-    
-    for key in averages:
-        value = averages[key]
-        value = value / len(iterationResults)
-        averages[key] = value
-        
-    dev = {}
-    
-    for result in iterationResults:
-        for keyvalue in result:
-            key = keyvalue[0]
-            value = keyvalue[1]
-            if key not in dev:
-                dev[key] = 0.0
-            
-            current = dev[key]
-            current = current + math.pow(value - averages[key], 2.0)
-            dev[key] = current
-    
-    for key in averages:
-        value = dev[key]
-        value = value / len(iterationResults)
-        value = math.sqrt(value)
-        dev[key] = value
-    
-    return {"avg": averages, "std": dev, "scatter": scatterResult }
+    return trainX, testX, trainY, testY
+
