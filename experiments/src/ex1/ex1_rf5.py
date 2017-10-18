fe51be36cddd6466bba6c0730ab61a4655e5516d
Gabor Makrai <makraigabor@gmail.com>
2017-10-18 18:51:45 +0100

    experiments: added ex1.rf5

---
+++ experiments/src/ex1/ex1_rf5.py
@@ -0,0 +1,60 @@
+from data.data import loadData
+from ex1.crossvalidation import splitDataForXValidation
+from copy import deepcopy
+from eval.rmse import rmseEval
+from sklearn.ensemble.forest import RandomForestRegressor
+
+OUTPUT_DATA_FILE = "/experiments/ex1/ex1_rf5.csv"
+
+parametersList = []
+
+for n in range(5,1000):
+    for leaf in [2,3,4]:
+        parametersList.append({"n_estimators": n, "leaf": leaf})
+    
+locations = [2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0]
+
+# load the data
+data = {}
+columns = []
+loadData("/data/york_hour_2013.csv", ["timestamp", "atc"], data, columns)
+
+all_features = deepcopy(columns)
+all_features.remove("target")
+all_features.remove("location")
+
+output = open(OUTPUT_DATA_FILE, 'w')
+output.write("method,value,rmse\n")
+
+def evalOne(parameters):
+    all_obs = []
+    all_pred = []
+    for location in locations:
+        trainX, testX, trainY, testY = splitDataForXValidation(location, "location", data, all_features, "target")
+        if "depth" in parameters:
+            model = RandomForestRegressor(max_depth = parameters["depth"], random_state=42, n_estimators=parameters["n_estimators"], n_jobs=3)
+        elif "leaf" in parameters:
+            model = RandomForestRegressor(min_samples_leaf = parameters["leaf"], random_state=42, n_estimators=parameters["n_estimators"], n_jobs=3)
+        elif "max_leaf" in parameters:
+            model = RandomForestRegressor(max_leaf_nodes = parameters["max_leaf"], random_state=42, n_estimators=parameters["n_estimators"], n_jobs=3)
+            
+        model.fit(trainX, trainY)
+        prediction = model.predict(testX)
+        all_obs.extend(testY)
+        all_pred.extend(prediction)
+    return rmseEval(all_obs, all_pred)[1]
+
+for p in parametersList:
+    print(str(p))
+    rmse = evalOne(p)
+    print("\t" + str(rmse))
+    if "depth" in p:
+        output.write(str(p["n_estimators"]) + ",depth," + str(p["depth"]) + "," + str(rmse) + "\n")
+    elif "leaf" in p:
+        output.write(str(p["n_estimators"]) + ",leaf," + str(p["leaf"]) + "," + str(rmse) + "\n")
+    elif "max_leaf" in p:
+        output.write(str(p["n_estimators"]) + ",max_leaf," + str(p["max_leaf"]) + "," + str(rmse) + "\n")
+    output.flush()
+    
+    
+output.close()
