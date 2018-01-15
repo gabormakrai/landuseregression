aaa66a9c65441cbfe159321dd037dbc66e67c7ce
Gabor Makrai <makraigabor@gmail.com>
2018-01-15 22:03:37 +0000

    experiments: updated ex2_summary script

--- experiments/src/ex2/ex2_summary.py
+++ experiments/src/ex2/ex2_summary.py
@@ -10,7 +10,28 @@ from sklearn.metrics import r2_score
 INPUT_DATA_DIRECTORY = "/experimntes/ex2/"
 OUTPUT_DIRECTORY = "/experiments/ex2/"
 
-methods = ["ospm", "linear_lu", "linear_all", "knn", "svm", "ann", "dt", "rf"]
+methods = ["ospm", "linear_lu", "linear_all", "knn", "ann", "svm", "dt", "rf"]
+
+methodNames = {
+    "ospm": "OSPM\nmethod",
+    "linear_lu": "Standard\nLUR",
+    "linear_all": "Linear\nRegression",
+    "knn": "Nearest Neighour\nRegression",
+    "svm": "Support Vector\nRegression",
+    "ann": "Neural Network\nRegression",
+    "dt": "Decision Tree\nRegression",
+    "rf": "Random Forest\nRegression"
+}
+
+methodNamesOneLine = {
+    "linear_lu": "Standard LUR",
+    "linear_all": "Linear Regression",
+    "knn": "Nearest Neighour Regression",
+    "svm": "Support Vector Regression",
+    "ann": "Neural Network Regression",
+    "dt": "Decision Tree Regression",
+    "rf": "Random Forest Regression"
+}
 
 stations = [2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0]
 stationNames = {2.0: "Fulford", 3.0: "Gillygate", 4.0: "Heworth", 5.0: "Holgate", 6.0: "Lawrence", 7.0: "Nunnery", 8.0: "Fishergate"}
@@ -38,15 +59,26 @@ for method in methods:
         observationsPerStation[method][l].append(o)
         predictionsNormal[method].append(p)
         observationsNormal[method].append(o)
+
+rmseLevels = {}
+maeLevels = {}
+rLevels = {}
     
 for method in methods:
     print("Method: " + method)
-    print("\trmse: " + str(rmseEval(observations[method], predictions[method])[1]))
-    print("\tmae: " + str(maeEval(observations[method], predictions[method])[1]))
-    print("\tr: " + str(correlationEval(observations[method], predictions[method])[1]))
+    rmse = rmseEval(observations[method], predictions[method])[1]
+    print("\trmse: " + str(rmse))
+    mae = maeEval(observations[method], predictions[method])[1]
+    print("\tmae: " + str(mae))
+    r = correlationEval(observations[method], predictions[method])[1]
+    print("\tr: " + str(r))
     print("\tr2: " + str(rsquaredEval(observations[method], predictions[method])[1]))
     print("\tr2: " + str(r2_score(observations[method], predictions[method])))
-     
+    
+    rmseLevels[method] = str(rmse)[0:5]
+    maeLevels[method] = str(mae)[0:5]
+    rLevels[method] = str(r)[0:4]
+      
     fig = plt.figure(figsize=(5.76, 5.76))
     ax = fig.add_subplot(111)
     ax.scatter(observationsNormal[method], predictionsNormal[method], alpha=0.1)
@@ -56,7 +88,7 @@ for method in methods:
     plt.xlabel("Observation (ug/m3)")
     plt.savefig(OUTPUT_DIRECTORY + "ex2_" + method + ".png")
     plt.close()
- 
+  
     fig = plt.figure(figsize=(10, 10))
     ax = fig.add_subplot(111)
     for s in stations:
@@ -70,16 +102,37 @@ for method in methods:
         lh.set_alpha(1.0)
     plt.savefig(OUTPUT_DIRECTORY + "ex2_" + method + "_perstation.png")
     plt.close()
+    
+    if method != "ospm":
+        fig = plt.figure(figsize=(5.76, 5.76))
+        ax = fig.add_subplot(111)
+        ax.scatter(observationsNormal["ospm"], predictionsNormal["ospm"], alpha=0.02, label="OSPM",c='g')
+        ax.scatter(observationsNormal[method], predictionsNormal[method], alpha=0.02, label=methodNamesOneLine[method],c='r')
+        plt.xlim(0,150)
+        plt.ylim(0,150)
+        plt.ylabel("Prediction (ug/m3)")
+        plt.xlabel("Observation (ug/m3)")
+        leg = plt.legend()
+        for lh in leg.legendHandles: 
+            lh.set_alpha(1.0)
+        plt.savefig(OUTPUT_DIRECTORY + "ex2_" + method + "_vs_ospm.png")
+        plt.close()
 
-fig = plt.figure(figsize=(10, 10))
+fig = plt.figure(figsize=(13.0, 8.0))
 ax = fig.add_subplot(111)
 labels = []
 dataToPlot = []
 for method in methods:
-    labels.append(method)
+    label = methodNames[method]
+    label = label + "\n" + "RMSE:" + rmseLevels[method] 
+    label = label + "\n" + "MAE:" + maeLevels[method] 
+    label = label + "\n" + "r:" + rLevels[method] 
+    labels.append(label)
     d = [abs(observations[method][i] - predictions[method][i]) for i in range(0, len(observations[method]))]
     dataToPlot.append(d)
 ax.boxplot(dataToPlot, showfliers=False)
 ax.set_xticklabels(labels)
-plt.savefig(OUTPUT_DIRECTORY + "ex2_plot.png")
+plt.ylabel("Absolute error (ug/m3)")
+plt.subplots_adjust(bottom=0.2)
+plt.savefig(OUTPUT_DIRECTORY + "ex2_absolute_error_boxplot.png")
 plt.close()
