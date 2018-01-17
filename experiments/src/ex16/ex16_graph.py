efb0f44e5f4518ee77e68896242642913fa0d3fe
Gabor Makrai <makraigabor@gmail.com>
2018-01-17 21:52:24 +0000

    experiments: updated ex16_graph script

--- experiments/src/ex16/ex16_graph.py
+++ experiments/src/ex16/ex16_graph.py
@@ -37,16 +37,18 @@ all_features = []
 all_features.extend(['leisure_area', 'landuse_area'])
 all_features.extend(['buildings_number', 'buildings_area'])
 all_features.extend(['lane_length', 'length'])
-all_features.extend(['traffic_length_car', 'traffic_length_lgv', 'traffic_length_hgv'])
+all_features.extend(['atc'])
 all_features.extend(['winddirection', 'windspeed', 'temperature', 'rain', 'pressure'])
 all_features.extend(['hour', 'day_of_week', 'month', 'bank_holiday', 'race_day'])
 
-global_best_result = 11.969831832942312
-global_best_step = [False, False, False, False, False, False, False, False, False, False, True, True, False, True, True, True, False, True, False]
+global_best_result = 12.613385504601814
+global_best_step = [True, False, False, False, False, False, False, False, True, True, True, True, True, True, False, True, False]
 
 print("Best RMSE: " + str(global_best_result))
 print("Best Step: " + str(global_best_step))
 best_features = [all_features[i] for i in range(0, len(all_features)) if global_best_step[i]]  
 print("Best features:" + str(best_features))
+
+
 
 
