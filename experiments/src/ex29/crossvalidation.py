2cd0561d44b10a0eeeee70dad06521a2ec38469b
Gabor Makrai <makraigabor@gmail.com>
2018-02-07 00:20:19 +0000

    experiments: updated ex29.crossvalidation script

--- experiments/src/ex29/crossvalidation.py
+++ experiments/src/ex29/crossvalidation.py
@@ -32,3 +32,42 @@ def splitDataForXValidation(trainSet, testSet, validationColumn, data, columns, 
         targetX.append(X)
             
     return trainX, testX, trainY, testY
+
+def splitDataForXValidationWithLocation(trainSet, testSet, validationColumn, data, columns, targetColumn):
+    
+    columnsWithoutValidationAndTarget = deepcopy(columns)
+    if validationColumn in columnsWithoutValidationAndTarget:
+        columnsWithoutValidationAndTarget.remove(validationColumn)
+    if targetColumn in columnsWithoutValidationAndTarget:
+        columnsWithoutValidationAndTarget.remove(targetColumn)
+    
+    trainX = []
+    testX = []
+    trainY = []
+    testY = []
+    trainLocation = []
+    testLocation = []
+    
+    for i in range(0, len(data[validationColumn])):
+        targetX = trainX
+        targetY = trainY
+        targetLocation = trainLocation
+        if data[validationColumn][i] in testSet:
+            targetX = testX
+            targetY = testY
+            targetLocation = testLocation
+        elif data[validationColumn][i] not in trainSet:
+            continue
+            
+        X = []
+        for c in columnsWithoutValidationAndTarget:
+            X.append(data[c][i])
+            
+        Y = data[targetColumn][i]
+        l = data["location"][i]
+        
+        targetY.append(Y)
+        targetX.append(X)
+        targetLocation.append(l)
+            
+    return trainX, testX, trainY, testY, trainLocation, testLocation
