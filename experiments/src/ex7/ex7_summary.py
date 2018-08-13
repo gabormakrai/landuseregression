26001cbbf323e12f364c4e544f72493775db1f3d
Gabor Makrai <makraigabor@gmail.com>
2018-08-13 23:16:46 +0100

    experiments: updated ex7_summary to pick up new metrics

--- experiments/src/ex7/ex7_summary.py
+++ experiments/src/ex7/ex7_summary.py
@@ -6,6 +6,11 @@ from eval.correlation import correlationEval
 from eval.rsquared import rsquaredEval
 from collections import defaultdict
 from sklearn.metrics import r2_score
+from eval.fac2 import fac2Eval
+from eval.mg import mgEval
+from eval.nmse import nmse_from_paper
+from eval.fb import fbEval
+from eval.random_scatter import evalRandomScatter
 
 INPUT_DATA_DIRECTORY = "/experimntes/ex7/"
 OUTPUT_DIRECTORY = "/experiments/ex7/"
@@ -54,6 +59,10 @@ for method in methods:
 rmseLevels = {}
 maeLevels = {}
 rLevels = {}
+fac2Levels = {}
+nmseLevels = {}
+fbLevels = {}
+rsLevels = {}
     
 for method in methods:
     print("Method: " + method)
@@ -65,21 +74,55 @@ for method in methods:
     print("\tr: " + str(r))
     print("\tr2: " + str(rsquaredEval(observations[method], predictions[method])[1]))
     print("\tr2: " + str(r2_score(observations[method], predictions[method])))
+    fac2 = fac2Eval(observations[method], predictions[method])
+    print("\tfac2: " + str(fac2))
+    print("\tmg: " + str(mgEval(observations[method], predictions[method])))
+    nmse = nmse_from_paper(observations[method], predictions[method])
+    print("\tnmse: " + str(nmse))
+    fb = fbEval(observations[method], predictions[method])[1] 
+    print("\tfb: " + str(fb))
+    rs = evalRandomScatter(observations[method], predictions[method])
+    print("\trandom_scatter: " + str(rs))
     
-    rmseLevels[method] = str(rmse)[0:5]
-    maeLevels[method] = str(mae)[0:5]
+    rmseLevels[method] = str(rmse)[0:4]
+    maeLevels[method] = str(mae)[0:4]
     rLevels[method] = str(r)[0:4]
+    fac2Levels[method] = str(fac2)[0:4]
+    nmseLevels[method] = str(nmse)[0:4]
+    if fb < 0.01:
+        fbLevels[method] = '0.00'
+    else:
+        fbLevels[method] = str(fb)[0:4]
+    if fbLevels[method] == '-0.00':
+        fbLevels[method] = '0.00'
+    rsLevels[method] = str(rs)[0:4]
       
     fig = plt.figure(figsize=(5.76, 5.76))
     ax = fig.add_subplot(111)
-    ax.scatter(observationsNormal[method], predictionsNormal[method], alpha=0.1)
+    ax.scatter(observationsNormal[method], predictionsNormal[method], alpha=0.1, label="Prediction-observation pairs")
+    ax.plot([0,150], [0, 75], color='red', label="FAC2 lines")
+    ax.plot([0,75], [0, 150], color='red')
     plt.xlim(0,150)
     plt.ylim(0,150)
     plt.ylabel(r'Prediction ($\mu$gm${}^{-3}$)')
     plt.xlabel(r'Observation ($\mu$gm${}^{-3}$)')
+    plt.text(100, 143, "RMSE: " + rmseLevels[method])
+    plt.text(127, 143, "$\mu$gm${}^{-3}$")
+    plt.text(100, 137, "R: " + rLevels[method])
+    plt.text(100, 131, "FAC2: " + fac2Levels[method])
+    plt.text(100, 125, "NMSE: " + nmseLevels[method])
+    plt.text(100, 119, "FB: " + fbLevels[method])
+    plt.text(100, 113, "RS: " + rsLevels[method])
+      
+    leg = plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
+           ncol=2, mode="expand", borderaxespad=0.)
+      
+    for lh in leg.legendHandles: 
+        lh.set_alpha(1.0)
+          
     plt.savefig(OUTPUT_DIRECTORY + "ex7_" + method + ".png")
     plt.close()
-  
+   
     fig = plt.figure(figsize=(10, 10))
     ax = fig.add_subplot(111)
     for s in stations:
@@ -93,7 +136,7 @@ for method in methods:
         lh.set_alpha(1.0)
     plt.savefig(OUTPUT_DIRECTORY + "ex7_" + method + "_perstation.png")
     plt.close()
-    
+     
     if method != "ospm":
         fig = plt.figure(figsize=(5.76, 5.76))
         ax = fig.add_subplot(111)
@@ -115,15 +158,25 @@ labels = []
 dataToPlot = []
 for method in methods:
     label = methodNames[method]
-    label = label + "\n" + "RMSE:" + rmseLevels[method] 
-    label = label + "\n" + "MAE:" + maeLevels[method] 
-    label = label + "\n" + "r:" + rLevels[method] 
+    label = label + "\n" + "RMSE:" + rmseLevels[method] #+ "$\mu$gm${}^{-3}$" 
+    label = label + "\n" + "MAE:" + maeLevels[method] #+ "$\mu$gm${}^{-3}$"
+    label = label + "\n" + "R:" + rLevels[method]
+    label = label + "\n" + "FAC2:" + fac2Levels[method]
+    label = label + "\n" + "NMSE:" + nmseLevels[method]
+    label = label + "\n" + "FB:" + fbLevels[method]
+    label = label + "\n" + "RS:" + rsLevels[method]
     labels.append(label)
     d = [abs(observations[method][i] - predictions[method][i]) for i in range(0, len(observations[method]))]
     dataToPlot.append(d)
 ax.boxplot(dataToPlot, showfliers=False)
 ax.set_xticklabels(labels)
 plt.ylabel(r'Absolute error ($\mu$gm${}^{-3}$)')
-plt.subplots_adjust(bottom=0.2)
+plt.subplots_adjust(bottom=0.3)
+plt.text(1.15, -9.1, "$\mu$gm${}^{-3}$", fontsize=8)
+plt.text(1.13, -11.3, "$\mu$gm${}^{-3}$", fontsize=8)
+plt.text(2.15, -9.1, "$\mu$gm${}^{-3}$", fontsize=8)
+plt.text(2.13, -11.3, "$\mu$gm${}^{-3}$", fontsize=8)
+plt.text(3.15, -9.1, "$\mu$gm${}^{-3}$", fontsize=8)
+plt.text(3.13, -11.3, "$\mu$gm${}^{-3}$", fontsize=8)
 plt.savefig(OUTPUT_DIRECTORY + "ex7_absolute_error_boxplot.png")
 plt.close()
