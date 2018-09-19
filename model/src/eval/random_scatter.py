6fa10cda4adf7acaed74ba5f579d7503cb46450d
Gabor Makrai <makraigabor@gmail.com>
2018-09-19 23:45:30 +0100

    model: updated random_scatter calculation code

--- model/src/eval/random_scatter.py
+++ model/src/eval/random_scatter.py
@@ -9,7 +9,7 @@ def evalRandomScatter(targetData, predictionData):
         o = targetData[i]
         if p == 0.0 or p < 0.0:
             p = 0.001
-        if o == 0.0:
+        if o == 0.0 or o < 0.0:
             o = 0.001
             
         sum_target_prediction_squared = sum_target_prediction_squared + math.pow(math.log(o) - math.log(p), 2.0) 
